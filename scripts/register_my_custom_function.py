fae3f81f83ce8152edb7d7893600687b0ceefdb5
William Young <william.young@ibm.com>
2021-12-16 14:03:57 -0600

    update

---
+++ scripts/register_my_custom_function.py
@@ -0,0 +1,38 @@
+import datetime as dt
+import json
+import pandas as pd
+import numpy as np
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions.base import BaseTransformer
+from iotfunctions.metadata import EntityType
+from iotfunctions.db import Database
+from iotfunctions import ui
+
+# <><><><>
+
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+# <><><><>
+
+print("got imports");
+with open('credentials_as.json', encoding='utf-8') as F:
+  credentials = json.loads(F.read())
+print("Loaded creds")
+db_schema = None
+print(credentials);
+db = Database(credentials=credentials, echo=True)
+print("setup db");
+from customWY.multiplybyfactorWY import MultiplyByFactorWY
+print("registering");
+db.register_functions([MultiplyByFactorWY])
+print("registered");
