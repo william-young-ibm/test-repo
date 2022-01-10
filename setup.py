818e5becaeaca502b2fa69822980a1970abdaedc
William Young <william.young@ibm.com>
2022-01-10 04:35:05 -0600

    Fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codytest',
-    version='0.0.16',
+    version='0.0.17',
     packages=find_packages(),
     install_requires=[
         'dill==0.3.0',
